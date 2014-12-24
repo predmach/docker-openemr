1c5004b4a411a91c03584a6375c964943aa59522
Angel Rodriguez-Ricardo <angelrr7702@gmail.com>
2014-12-24 11:27:40 -0500

    Update pre-conf.sh

--- pre-conf.sh
+++ pre-conf.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 
+#downloading software openemr
 wget http://sourceforge.net/projects/openemr/files/OpenEMR%20Current/4.1.2.7/openemr-4.1.2.tar.gz
 tar -pxvzf openemr-4.1.2.tar.gz
 rm openemr-4.1.2.tar.gz
 mv openemr-4.1.2 /var/www/openemr
-cd /var/www
 
 #remove the basic page for apache
 rm -R /var/www/html
@@ -14,7 +14,5 @@ rm /etc/apache2/sites-enabled/000-default.conf
  ln -s /etc/apache2/conf-available/fqdn.conf /etc/apache2/conf-enabled/fqdn.conf
  #disable http
  sed -i 's/Listen 80/#Listen 80/' /etc/apache2/ports.conf
- #enabling ssl  , maybe need to move enabling to startup 
- a2enmod ssl
+ #for ssl, enabling move to startup.sh script .
  sed -i 's/DocumentRoot \/var\/www\/html/DocumentRoot \/var\/www\/openemr/' /etc/apache2/sites-available/default-ssl.conf
- a2ensite default-ssl
