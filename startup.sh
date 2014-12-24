fe1dadfa944ca0f185cc4ac8594bfddb5153440b
Angel Rodriguez-Ricardo <angelrr7702@gmail.com>
2014-12-24 11:25:47 -0500

    Update startup.sh

--- startup.sh
+++ startup.sh
@@ -6,6 +6,8 @@ if [ -f /etc/configured ]; then
   echo 'already configured'
 else
   #code that need to run only one time ....
+   a2enmod ssl
+   a2ensite default-ssl
   #needed for fix problem with ubuntu and cron
   update-locale
   date > /etc/configured
