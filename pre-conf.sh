38ef0c09ca0caece319fe4f342eede7c1dc0e314
Angel Rodriguez-Ricardo <angelrr7702@gmail.com>
2014-12-23 23:28:14 -0500

    Update pre-conf.sh

--- pre-conf.sh
+++ pre-conf.sh
@@ -1,4 +1,8 @@
 #!/bin/bash
-#reason of this script is that dockerfile only execute one command at the time but we need sometimes at the moment we create
-#the docker image to run more that one software for expecified configuration like when you need mysql running to chnage or create
-#database for the container ...
+
+
+wget http://sourceforge.net/projects/openemr/files/OpenEMR%20Current/4.1.2.7/openemr-4.1.2.tar.gz
+tar -pxvzf openemr-4.1.2.tar.gz
+rm openemr-4.1.2.tar.gz
+mv openemr-4.1.2 /var/www/openemr
+cd /var/www
