5f8253dbbc57412fa6ffabaf207bbcff1c1d14f6
Angel Rodriguez-Ricardo <angelrr7702@gmail.com>
2014-12-23 21:24:48 -0500

    Create after_install.sh

---
+++ after_install.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+#need to be added after_install scritp to get password of default user koha_library
+
+
+    chmod 644 openemr/library/sqlconf.php 
+    chmod 600 openemr/acl_setup.php 
+    chmod 600 openemr/acl_upgrade.php 
+    chmod 600 openemr/sl_convert.php 
+    chmod 600 openemr/setup.php 
+    chmod 600 openemr/sql_upgrade.php 
+    chmod 600 openemr/gacl/setup.php 
+    chmod 600 openemr/ippf_upgrade.php - See more at: http://open-emr.org/wiki/index.php/OpenEMR_4.1.2_Linux_Installation#sthash.7Fre4B3r.dpuf
