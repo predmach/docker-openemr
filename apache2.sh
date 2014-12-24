b40f4a9734e1cb606371ae106c690f02ffb045f1
Angel Rodriguez-Ricardo <angelrr7702@gmail.com>
2014-12-23 21:16:04 -0500

    Create apache2.sh

---
+++ apache2.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+### In apache2.sh (make sure this file is chmod +x):
+# `/sbin/setuser www-data` runs the given command as the user `www-data`.
+# If you omit that part, the command will be run as root.
+
+sleep 5s
+
+read pid cmd state ppid pgrp session tty_nr tpgid rest < /proc/self/stat
+trap "kill -TERM -$pgrp; exit" EXIT TERM KILL SIGKILL SIGTERM SIGQUIT
+
+source /etc/apache2/envvars
+exec apache2ctl -D FOREGROUND >>/var/log/apache2.log 2>&1
