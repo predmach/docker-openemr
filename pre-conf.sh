f49ae84935c6d0de75dfadfc4474ca27fc73372f
Angel Rodriguez-Ricardo <angelrr7702@gmail.com>
2015-08-29 16:10:49 -0400

    Update pre-conf.sh

--- pre-conf.sh
+++ pre-conf.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 #downloading software openemr
-wget http://sourceforge.net/projects/openemr/files/OpenEMR%20Current/4.2.0/openemr-4.2.0.tar.gz
+wget http://sourceforge.net/projects/openemr/files/OpenEMR%20Current/4.2.0.3/openemr-4.2.0.tar.gz
 tar -pxvzf openemr-4.2.0.tar.gz
 rm openemr-4.2.0.tar.gz
 mv openemr-4.2.0 /var/www/openemr
@@ -10,7 +10,7 @@ mv openemr-4.2.0 /var/www/openemr
 rm -R /var/www/html
 rm /etc/apache2/sites-enabled/000-default.conf
  #to fix error relate to ip address of container  apache2
- echo "ServerName localhost" | sudo tee /etc/apache2/conf-available/fqdn.conf
+ echo "ServerName localhost" | tee /etc/apache2/conf-available/fqdn.conf
  ln -s /etc/apache2/conf-available/fqdn.conf /etc/apache2/conf-enabled/fqdn.conf
  #disable http
  sed -i 's/Listen 80/#Listen 80/' /etc/apache2/ports.conf
