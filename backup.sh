a1c1807a36aefb0601edbd122042c28a3144b09c
Angel Rodriguez-Ricardo <angelrr7702@gmail.com>
2015-08-29 20:18:21 -0400

    Update backup.sh

--- backup.sh
+++ backup.sh
@@ -2,9 +2,6 @@
 #this for reference only to create the backup scrips for each container ... the idea to use the same command for each container
 #each container will have their own custum backup scritp for it ...
 
-#Backup mysql
-mysqldump -u root -pmysqlpsswd --all-databases > /var/backups/alldb_backup.sql
-
 #Backup important file ... of the configuration ...
 cp /etc/hosts /var/backups/
 
