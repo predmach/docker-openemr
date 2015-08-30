f6433bb325d3480a69703d5d27ffa8487d98ff3c
Angel Rodriguez-Ricardo <angelrr7702@gmail.com>
2015-08-30 08:48:34 -0400

    Update startup.sh

--- startup.sh
+++ startup.sh
@@ -9,6 +9,16 @@ else
    a2enmod ssl
    a2ensite default-ssl
    a2enmod rewrite
+   
+   chown -R www-data:www-data /var/www/openemr/gacl/admin/templates_c
+   chown -R www-data:www-data /var/www/openemr/sites/default/edi
+   chown -R www-data:www-data /var/www/openemr/sites/default/era
+   chown -R www-data:www-data /var/www/openemr/sites/default/documents
+   chown -R www-data:www-data /var/www/openemr/sites/default/letter_templates
+   chown -R www-data:www-data /var/www/openemr/library/freeb
+   chown -R www-data:www-data /var/www/openemr/interface/main/calendar/modules/PostCalendar/pntemplates/compiled
+   chown -R www-data:www-data /var/www/openemr/interface/main/calendar/modules/PostCalendar/pntemplates/cache
+   
   #needed for fix problem with ubuntu and cron
   update-locale
   date > /etc/configured
