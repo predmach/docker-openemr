395c69d058ecdb673d33217143ae2a29eaa3ef6a
Angel Rodriguez-Ricardo <angelrr7702@gmail.com>
2015-08-29 23:35:40 -0400

    Update startup.sh

--- startup.sh
+++ startup.sh
@@ -8,6 +8,7 @@ else
   #code that need to run only one time ....
    a2enmod ssl
    a2ensite default-ssl
+   a2enmod rewrite
   #needed for fix problem with ubuntu and cron
   update-locale
   date > /etc/configured
