d0e56f79b2dbb7f434a2a6375f271564dbc39840
Angel Rodriguez-Ricardo <angelrr7702@gmail.com>
2016-04-16 19:34:08 -0400

    Update pre-conf.sh

--- pre-conf.sh
+++ pre-conf.sh
@@ -10,11 +10,8 @@ chown -R www-data:www-data /var/www/openemr
 
 #remove the basic page for apache
 rm -R /var/www/html
-rm /etc/apache2/sites-enabled/000-default.conf
  #to fix error relate to ip address of container  apache2
  echo "ServerName localhost" | tee /etc/apache2/conf-available/fqdn.conf
  ln -s /etc/apache2/conf-available/fqdn.conf /etc/apache2/conf-enabled/fqdn.conf
- #disable http
- sed -i 's/Listen 80/#Listen 80/' /etc/apache2/ports.conf
- #for ssl, enabling move to startup.sh script .
- sed -i 's/DocumentRoot \/var\/www\/html/DocumentRoot \/var\/www\/openemr/' /etc/apache2/sites-available/default-ssl.conf
+ #change document root
+ sed -i 's/DocumentRoot \/var\/www\/html/DocumentRoot \/var\/www\/openemr/' /etc/apache2/sites-available/000-default.conf
