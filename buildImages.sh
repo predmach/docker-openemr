f64fcba8b7de4b19d92b8a77d9b95eac32cae572
predmach <chuckpace@gmail.com>
2016-09-25 13:07:50 -0700

    Updated steps to build both images

--- buildImages.sh
+++ buildImages.sh
@@ -1,3 +1,10 @@
 #!/bin/bash
+docker rmi predmach/mysql-emr
+docker stop DB
+docker rm DB
 docker build -f Dockerfile-MySql -t predmach/mysql-emr .
+
+docker stop open-emr
+docker rm open-emr
+docker rmi predmach/open-emr
 docker build -f Dockerfile-EMR -t predmach/open-emr .
